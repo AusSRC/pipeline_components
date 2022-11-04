9b35170914b2cd480d27eeffa7ef55a63bf8d377
Austin Shen <austin.shen@csiro.au>
2022-11-04 12:28:15 +0800

    POSSUM-22: try except to continue with regridding where no overlap

--- hpx_tiles/casa_tiling.py
+++ hpx_tiles/casa_tiling.py
@@ -91,61 +91,66 @@ def main(argv):
         logging.info(f"Producing tile {i+1}/{len(crpix1)}")
         one_tile_start = time.time()
 
-        # this is how I will update the dictionary
-        template_header["csys"]["direction0"]["crpix"] = np.array([ra, dec])
+        try:
+            # this is how I will update the dictionary
+            template_header["csys"]["direction0"]["crpix"] = np.array([ra, dec])
 
-        if len(axis) == 4:
-            fourth_axis = axis[3]
-            if fourth_axis == "Frequency":
-                number_of_frequency = fitsheader["shape"][3]
-                template_header["shap"] = np.array(
-                    [naxis, naxis, 1, number_of_frequency]
-                )
+            if len(axis) == 4:
+                fourth_axis = axis[3]
+                if fourth_axis == "Frequency":
+                    number_of_frequency = fitsheader["shape"][3]
+                    template_header["shap"] = np.array(
+                        [naxis, naxis, 1, number_of_frequency]
+                    )
 
-            third_axis = axis[2]
-            if third_axis == "Frequency":
-                number_of_frequency = fitsheader["shape"][2]
-                template_header["shap"] = np.array(
-                    [naxis, naxis, number_of_frequency, 1]
-                )
+                third_axis = axis[2]
+                if third_axis == "Frequency":
+                    number_of_frequency = fitsheader["shape"][2]
+                    template_header["shap"] = np.array(
+                        [naxis, naxis, number_of_frequency, 1]
+                    )
 
-        if len(axis) == 3:
-            third_axis = axis[2]
-            if third_axis == "Frequency":
-                number_of_frequency = fitsheader["shape"][2]
-                template_header["shap"] = np.array(
-                    [naxis, naxis, number_of_frequency]
-                )
-            else:
-                template_header["shap"] = np.array([naxis, naxis, 1])
+            if len(axis) == 3:
+                third_axis = axis[2]
+                if third_axis == "Frequency":
+                    number_of_frequency = fitsheader["shape"][2]
+                    template_header["shap"] = np.array(
+                        [naxis, naxis, number_of_frequency]
+                    )
+                else:
+                    template_header["shap"] = np.array([naxis, naxis, 1])
 
-        outputname = args.output + "%s-%s-%d.image" % (output_prefix, sbid, pixel_ID[i])
+            outputname = args.output + "%s-%s-%d.image" % (output_prefix, sbid, pixel_ID[i])
 
-        # tiling, outputs tile fits in CASA image.
-        imregrid(
-            imagename=image,
-            template=template_header,
-            output=outputname,
-            axes=[0, 1],
-            interpolation="cubic",
-            overwrite=True,
-        )
-        # convert casa image to fits image
-        one_tile_end = time.time()
-        logging.info(
-            "Tiling of pixel ID %d completed. Time elapsed %.3f seconds. "
-            % (pixel_ID[i], (one_tile_end - one_tile_start))
-        )
+            # tiling, outputs tile fits in CASA image.
+            imregrid(
+                imagename=image,
+                template=template_header,
+                output=outputname,
+                axes=[0, 1],
+                interpolation="cubic",
+                overwrite=True,
+            )
+            # convert casa image to fits image
+            one_tile_end = time.time()
+            logging.info(
+                "Tiling of pixel ID %d completed. Time elapsed %.3f seconds. "
+                % (pixel_ID[i], (one_tile_end - one_tile_start))
+            )
 
-        logging.info("Converting the casa image to fits image.")
-        exportfits(
-            imagename=outputname,
-            fitsimage=outputname.split(".image")[0] + ".fits",
-            overwrite=True
-        )
-        # delete all casa image files.
-        logging.info("Deleting the casa image. ")
-        os.system("rm -rf %s" % outputname)
+            logging.info("Converting the casa image to fits image.")
+            exportfits(
+                imagename=outputname,
+                fitsimage=outputname.split(".image")[0] + ".fits",
+                overwrite=True
+            )
+            # delete all casa image files.
+            logging.info("Deleting the casa image. ")
+            os.system("rm -rf %s" % outputname)
+        except Exception as e:
+            logging.error(f'There was an exception: {e}')
+            logging.info('Skipping pixel')
+            # TODO: need to update csv file
 
     end_tiling = time.time()
     logging.info(
