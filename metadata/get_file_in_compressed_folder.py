32362dd74750f5b25b409d4096e744bcf2a9b24e
Austin Shen <austin.shen@csiro.au>
2023-01-03 04:36:27 +0000

    Generic extract from compressed folder refactor

--- metadata/get_file_in_compressed_folder.py
+++ metadata/get_file_in_compressed_folder.py
@@ -8,7 +8,7 @@ import argparse
 import logging
 
 
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(level=logging.INFO)
 
 
 def parse_args(argv):
@@ -33,7 +33,7 @@ def parse_args(argv):
         '--keyword',
         type=str,
         required=False,
-        help='Search key word for identifying footprint files',
+        help='Search key word for identifying file of interest',
         default='metadata/footprintOutput'
     )
     args = parser.parse_args(argv)
@@ -53,32 +53,40 @@ def main(argv):
     args = parse_args(argv)
     if not os.path.exists(args.path):
         raise Exception(f'Path to files {args.path} does not exist.')
-    logging.info(f'Looking for footprint files in path "{args.path}" with keyword "{args.file}"')
+    logging.info(f'Looking for files in path "{args.path}" with filename matching "{args.file}"')
 
-    footprint = None
+    match_file = None
+    filename = None
+
     filelist = glob.glob(f'{args.path}/*{args.file}*')
+    if not filelist:
+        raise Exception(f'No files found in {args.path} with compressed file matching {args.file}')
+
     tarfiles = [f for f in filelist if (('checksum' not in f) and ('.tar' in f))]
+    if not tarfiles:
+        raise Exception(f'No compressed files found in {args.path} with filename matching {args.file}')
+
     logging.info(f'Compressed files: {tarfiles}')
     for tf in tarfiles:
         with tarfile.open(tf) as tar:
             logging.info(tf)
             files = [ti.name for ti in tar.getmembers() if args.keyword in ti.name]
             if files:
-                footprint = files[0]
+                match_file = files[0]
+                filename = os.path.join(args.path, match_file)
 
-            logging.info(f'Extracting tar file at {tf}')
-            tar.extractall()
-            logging.info(f'Searching for "{args.keyword}" in files: {files}')
-        if footprint:
+            # If file already exists do not extract from compressed file
+            if not os.path.exists(filename):
+                logging.info(f'Extracting tar file at {tf}')
+                tar.extractall(path=args.path)
+        if match_file:
             break
 
-    if not footprint:
+    if not match_file:
         raise Exception('No footprint files found in the evaluation files.')
-
-    # Check footprint file exists as expected
-    footprint_file = os.path.join(args.path, footprint)
-    os.path.exists(footprint_file)
-    print(footprint_file, end='')
+    if not os.path.exists(filename):
+        raise Exception(f'No file found {filename}')
+    print(filename, end='')
     return
 
 
