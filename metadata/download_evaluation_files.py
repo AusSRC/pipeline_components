2b0873a6535a74a75d06fd043385272cc8f2ee81
davepallot <dave.pallot@uwa.edu.au>
2023-10-26 06:09:26 +0000

    Dont raise exception

--- metadata/download_evaluation_files.py
+++ metadata/download_evaluation_files.py
@@ -26,26 +26,22 @@ EVAL_URL = "https://data.csiro.au/casda_vo_proxy/vo/datalink/links?ID="
 def parse_args(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-s", "--sbid", type=int, required=True, help="SBID for observation"
-    )
+        "-s", "--sbid", type=int, required=True, help="SBID for observation")
     parser.add_argument(
-        "-p", "--project_code", type=str, required=True, help="Project code"
-    )
+        "-p", "--project_code", type=str, required=True, help="Project code")
     parser.add_argument(
         "-o",
         "--output",
         type=str,
         required=True,
-        help="Output directory for metadata files.",
-    )
+        help="Output directory for metadata files.",)
     parser.add_argument(
         "-c",
         "--credentials",
         type=str,
         required=False,
         help="CASDA credentials config file.",
-        default="./casda.ini",
-    )
+        default="./casda.ini",)
     args = parser.parse_args(argv)
     return args
 
@@ -68,26 +64,31 @@ def download_evaluation_files(sbid, project_code, username, password, output):
     res = requests.get(url)
     if res.status_code != 200:
         raise Exception(f"Response: {res.reason} {res.status_code}")
+
     logging.info(f"Response: {res.json()}")
 
     # TODO(austin): what does it mean to have multiple evaluation files?
     evaluation_files = [f for f in res.json() if "evaluation" in f]
     evaluation_files.sort()
     if not evaluation_files:
-        raise Exception(
-            f"No evaluation files found with query parameters projectCode={project_code} and sbid={sbid}"
-        )
+        logging.warn(f"No evaluation files found with query parameters projectCode={project_code} and sbid={sbid}")
+        return
 
     # Download evaluation files
     casda = Casda(username, password)
 
     # TODO(austin): may not be necessary to download all evaluation files at once
     logging.info(f"Downloading evaluation files: {evaluation_files}")
+
     t = Table()
     t["access_url"] = [f"{EVAL_URL}{f}" for f in evaluation_files]
+
     logging.info(f"Downloading from: {t}")
+
     url_list = casda.stage_data(t)
+
     logging.info(f"Staging files {url_list}")
+
     download_url_list = []
     for url in url_list:
         filename = url.split("?")[0].rsplit("/", 1)[1]
@@ -110,8 +111,7 @@ def main(argv):
         args.project_code,
         casda_parser["CASDA"]["username"],
         casda_parser["CASDA"]["password"],
-        args.output,
-    )
+        args.output,)
 
 
 if __name__ == "__main__":
